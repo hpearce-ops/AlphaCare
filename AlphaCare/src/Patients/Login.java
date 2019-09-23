6e1b732be0fd3885dc09de01a3b29a769d4658e2
hpearce-ops <53911102+hpearce-ops@users.noreply.github.com>
2019-09-22 21:16:03 -0400

    Added login.java to Patients

---
+++ AlphaCare/src/Patients/Login.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Patients;
+
+/**
+ *
+ * @author henrypearce
+ */
+public class Login {
+
+    private Patient patient;
+
+    /**
+     *
+     * This is the default constructor for login.
+     * @param patient
+     */
+    public Login(Patient patient) {
+        this.patient = patient;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Patient getPatient() {
+        return patient;
+    }
+
+    /**
+     *
+     * @param patient
+     */
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    
+    /**
+     * Checks to see if the user input is valid. 
+     * @param patient
+     * @return Will return false if check fails and true if check succeeds. 
+     */
+    public Boolean authenticateUser(Patient patient){
+        return true || false; 
+    }
+    
+
+}
