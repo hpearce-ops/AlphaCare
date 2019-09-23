049868a34038e9e5941a95f9210059cefc21a2b5
hpearce-ops <53911102+hpearce-ops@users.noreply.github.com>
2019-09-22 21:34:52 -0400

    Updated AlphaCare.java

--- AlphaCare/src/Main/AlphaCare.java
+++ AlphaCare/src/Main/AlphaCare.java
@@ -10,12 +10,25 @@ package Main;
  * @author henrypearce
  */
 public class AlphaCare {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        int selection = 0; 
+        //listeners will determine which button user presses. 
+        selectUserAPI(selection); 
+    }
+    
+    /**
+     * Based on an integer value, the method will call the login method for the desired API.
+     * 1 = Administrator
+     * 2 = Medical Personnel
+     * 3 = Patient
+     * @param selection
+     */
+    public static void selectUserAPI(int selection) {
+        String API = "";  
+        //if statements and other login method calls (methods can be found in respective packages). 
     }
     
 }
