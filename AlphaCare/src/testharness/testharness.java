c75e1671c2c343f6f70d13553ded777c3f7ddb47
David Schmidt <dms6513@psu.edu>
2019-10-06 22:08:09 -0400

    created testharness module and class

---
+++ AlphaCare/src/testharness/testharness.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testharness;
+
+import static Main.AlphaCare.selectUserAPI;
+
+/**
+ *
+ * @author david
+ */
+public class testharness {
+        public static void main(String[] args) {
+        int selection = 0; 
+        //listeners will determine which button user presses. 
+        selectUserAPI(selection); 
+    }
+}
