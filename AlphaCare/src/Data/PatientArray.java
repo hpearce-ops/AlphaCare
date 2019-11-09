3f34e4eeeb630501ac5dbcd15aa3f90f1055b439
hpearce-ops <53911102+hpearce-ops@users.noreply.github.com>
2019-11-09 17:43:09 -0500

    Added short term package for data storage.
    Useful for test cases

---
+++ AlphaCare/src/Data/PatientArray.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+
+import Model.Patients.Patient;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author henrypearce
+ */
+public class PatientArray {
+    ArrayList<Patient> patientList; 
+
+    public PatientArray() {
+        this.patientList = new ArrayList<>();
+        populateArray(); 
+    }
+    
+    private void populateArray() {
+        patientList.add(new Patient("John Doe", "12345678", "password1")); 
+        patientList.add(new Patient("Jane Doe", "87654321", "password2"));
+        patientList.add(new Patient("Robert Redford", "55555555", "password3"));
+        patientList.add(new Patient("Alice Wonderland", "89898989", "password4"));        
+    }
+    
+    public String[] getArrayNames() {
+        ArrayList<String> nameList = new ArrayList<>(); 
+        for (int i = 0; i < patientList.size(); i++) {
+            nameList.add(patientList.get(i).getPatientName()); 
+        }
+        String[] nameArray = nameList.toArray(new String[nameList.size()]); 
+        return nameArray; 
+    }
+
+    public ArrayList<Patient> getPatientList() {
+        return patientList;
+    }
+       
+}
