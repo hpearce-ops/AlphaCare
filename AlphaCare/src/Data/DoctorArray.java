d2ab583daa1d4bd2e707732f4d46fe1708026a86
accountAndrew <ajkow2010@gmail.com>
2019-11-10 20:12:03 -0500

    Filled in doc array Please enter the commit message for your changes. Lines starting

--- AlphaCare/src/Data/DoctorArray.java
+++ AlphaCare/src/Data/DoctorArray.java
@@ -5,10 +5,38 @@
  */
 package Data;
 
+import Model.Medical.MedicalPersonnel;
+import java.util.ArrayList;
+
 /**
  *
  * @author ajkow
  */
 public class DoctorArray {
-    
+
+    ArrayList<MedicalPersonnel> medicalList;
+
+    public DoctorArray() {
+        this.medicalList = new ArrayList<>();
+        fillDocList();
+    }
+
+    private void fillDocList() {
+        medicalList.add(new MedicalPersonnel("John Smith", "Freddy12"));
+        medicalList.add(new MedicalPersonnel("Floyd Collins", "FrKj563"));
+        medicalList.add(new MedicalPersonnel("Demi Gawd", "G6F2"));
+    }
+
+    public String[] getArrayDocs() {
+        ArrayList<String> nameList = new ArrayList<>();
+        for (int i = 0; i < medicalList.size(); i++) {
+            nameList.add(medicalList.get(i).getPersonnelID());
+        }
+        String[] nameArray = nameList.toArray(new String[nameList.size()]);
+        return nameArray;
+    }
+
+    public ArrayList getDocList() {
+        return medicalList;
+    }
 }
