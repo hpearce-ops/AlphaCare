ba20f7e8d64d373f500960cd0c1a3b41351da06f
Matt Draghi <mtd5264@gmail.com>
2019-11-10 20:50:51 -0500

    Update CreateUserController.java

--- AlphaCare/src/Controller/CreateUserController.java
+++ AlphaCare/src/Controller/CreateUserController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-ppackage Controller;
+package Controller;
 
 import static Controller.LoginController.loginView;
 import View.CreateUserView;
