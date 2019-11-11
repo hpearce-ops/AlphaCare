65545a069b9c21608de248277d0b1102d40a041f
hpearce-ops <53911102+hpearce-ops@users.noreply.github.com>
2019-11-10 21:01:36 -0500

    more updates

--- AlphaCare/src/Data/PatientArray.java
+++ AlphaCare/src/Data/PatientArray.java
@@ -5,7 +5,6 @@
  */
 package Data;
 
-
 import Model.Patients.Patient;
 import java.util.ArrayList;
 
@@ -14,31 +13,42 @@ import java.util.ArrayList;
  * @author henrypearce
  */
 public class PatientArray {
-    ArrayList<Patient> patientList; 
+
+    ArrayList<Patient> patientList;
 
     public PatientArray() {
         this.patientList = new ArrayList<>();
-        populateArray(); 
+        populateArray();
     }
-    
+
     private void populateArray() {
-        patientList.add(new Patient("John Doe", "12345678", "password1")); 
+        patientList.add(new Patient("John Doe", "12345678", "password1"));
         patientList.add(new Patient("Jane Doe", "87654321", "password2"));
         patientList.add(new Patient("Robert Redford", "55555555", "password3"));
-        patientList.add(new Patient("Alice Wonderland", "89898989", "password4"));        
+        patientList.add(new Patient("Alice Wonderland", "89898989", "password4"));
     }
-    
+
     public String[] getArrayNames() {
-        ArrayList<String> nameList = new ArrayList<>(); 
+        ArrayList<String> nameList = new ArrayList<>();
         for (int i = 0; i < patientList.size(); i++) {
-            nameList.add(patientList.get(i).getPatientName()); 
+            nameList.add(patientList.get(i).getPatientName());
         }
-        String[] nameArray = nameList.toArray(new String[nameList.size()]); 
-        return nameArray; 
+        String[] nameArray = nameList.toArray(new String[nameList.size()]);
+        return nameArray;
     }
 
     public ArrayList<Patient> getPatientList() {
         return patientList;
     }
-       
+
+    public Patient findPatient(String patName) {
+        Patient temp = null;
+        for (int i = 0; i < patientList.size(); i++) {
+            if (patName.equals(patientList.get(i).getPatientName())) {
+                temp = patientList.get(i);
+            }
+        }
+        return temp;
+    }
+
 }
