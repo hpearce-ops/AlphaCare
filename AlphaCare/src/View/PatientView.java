bc3495e1c2aa313de23af935eb3bf1cd48a80bf8
hpearce-ops <53911102+hpearce-ops@users.noreply.github.com>
2019-12-08 01:34:31 -0500

    implemented use case

--- AlphaCare/src/View/PatientView.java
+++ AlphaCare/src/View/PatientView.java
@@ -9,20 +9,16 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
-import java.util.Properties;
 import javax.swing.JFrame;
 import javax.swing.JList;
 
-import org.jdatepicker.impl.JDatePanelImpl;
-import org.jdatepicker.impl.JDatePickerImpl;
-import org.jdatepicker.impl.UtilDateModel;
-
 import Data.PatientApptArray;
-import Model.Admin.Appointment; 
-import Model.Medical.MedicalPersonnel; 
+import Model.Admin.Appointment;
+import Model.Medical.MedicalPersonnel;
 import Model.Patients.Patient;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import javax.swing.DefaultListModel;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -31,104 +27,121 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+
 /**
  *
  * @author david
  */
 public class PatientView extends AbstractView {
 
+    private final JList trueList;
+    private final JList falseList;
+    private final JButton undo; 
+
     public PatientView() {
         this.frame = new JFrame("Patient View");
-        this.patientAppts = new PatientApptArray(); 
-        this.appointmentList = new JList<>(patientAppts.appointmentStatusSort()); 
-        this.model = new UtilDateModel();
-        this.timeField = new JTextField();
-        this.enter = new JButton("Confirm"); 
-        this.exit = new JButton("Cancel"); 
-        Properties p = new Properties();
-        p.put("text.today", "Today");
-        p.put("text.month", "Month");
-        p.put("text.year", "Year");
-        this.datePanel = new JDatePanelImpl(model, p);
-        this.datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+        this.patientAppts = new PatientApptArray();
+        this.appointmentList = new JList<>(patientAppts.appointmentStatusSort());
+        this.trueList = new JList<>(patientAppts.getTrueList());
+        this.falseList = new JList<>(patientAppts.getFalseList());
+        this.enter = new JButton("Confirm");
+        this.exit = new JButton("Cancel");
+        this.undo = new JButton("Undo"); 
     }
-    
+
     @Override
     public void userSpecificUI() {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JPanel interactionPanel = new JPanel(new GridLayout(0,2)); 
-        JPanel appointmentPanel = new JPanel(new GridLayout(0,3));
-        model.setDate(2019, 11, 3);
-        model.setSelected(true);
-        JLabel waitingLabel = new JLabel("Waiting for Approval"); 
-        timeField.setText("Waiting for Approval");
-        ActionListener appointment = new AppointmentListener(); 
-        ActionListener calendar = new CalendarListener();
-        ListSelectionListener patient = new PatientListener();
+        JPanel interactionPanel = new JPanel(new GridLayout(0, 1));
+
+        JLabel waitingLabel = new JLabel("Waiting for Approval");
+        JLabel approveLabel = new JLabel("Approved Appointments");
+        JLabel cancelLabel = new JLabel("Cancelled Appointments");
+
+        ActionListener appointment = new AppointmentListener();
+
         enter.addActionListener(appointment);
-        datePicker.addActionListener(calendar);
-        doctorList.addListSelectionListener(patient);
-        interactionPanel.add(waitingLabel); 
+        exit.addActionListener(appointment);
+        undo.addActionListener(appointment);
+        
+        buttonPanel.add(enter);
+        buttonPanel.add(exit);
+        buttonPanel.add(undo);
+        
+        interactionPanel.add(waitingLabel);
         interactionPanel.add(appointmentList);
-        interactionPanel.add(enter);
-        interactionPanel.add(exit); 
-        interactionPanel.add(datePicker); 
-        interactionPanel.add(timeField); 
-        buttonPanel.add(enter); 
-        frame.getContentPane().add(interactionPanel, BorderLayout.NORTH); 
-        frame.getContentPane().add(buttonPanel, BorderLayout.CENTER);
-        
+        interactionPanel.add(buttonPanel);
+        interactionPanel.add(approveLabel);
+        interactionPanel.add(trueList);
+        interactionPanel.add(cancelLabel);
+        interactionPanel.add(falseList);
+
         JTabbedPane navPane = new JTabbedPane();
-        
+
         JPanel recordTab = new JPanel(); //medical record panel
         JLabel label1 = new JLabel();
         recordTab.add(label1);
-        
+
         JPanel apptTab = new JPanel(); //appointment panel
         apptTab.add(interactionPanel, BorderLayout.NORTH);
-        apptTab.add(buttonPanel, BorderLayout.CENTER);
-        apptTab.add(appointmentPanel, BorderLayout.SOUTH);
-        
+
         JPanel settingsTab = new JPanel(); //settings panel
         JLabel label3 = new JLabel();
-        label3.setText("Security Settings Area"); 
+        label3.setText("Security Settings Area");
         settingsTab.add(label3);
-        
+
         navPane.addTab("Medical Record", recordTab);
         navPane.addTab("Appointments", apptTab);
         navPane.addTab("Settings", settingsTab);
-        
-        frame.getContentPane().add(navPane); 
+
+        frame.getContentPane().add(navPane);
 
     }
 
     private class AppointmentListener implements ActionListener {
+
         @Override
         public void actionPerformed(ActionEvent e) {
-           Date selectedDate = (Date) datePicker.getModel().getValue();
-           String selectedName = (String) doctorList.getSelectedValue();
-           String selectedTime = (String) timeField.getText(); 
-           MedicalPersonnel DrCarder = new MedicalPersonnel("Jane Doe", "12345678");
-           Patient patient = new Patient(selectedName, "12345678", "password1"); 
-           Appointment apt = new Appointment(patient, DrCarder, selectedDate, selectedTime);
-           System.out.println(apt); 
-        }      
-    }
-    
-    private class CalendarListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            Date selectedDate = (Date) datePicker.getModel().getValue();
-            System.out.println(selectedDate);
+            if (e.getSource() == enter) {
+                String temp = (String) appointmentList.getSelectedValue();
+                Appointment apt = patientAppts.findAppointment(temp);
+                apt.setStatus(Boolean.TRUE);
+            } else if (e.getSource() == exit) {
+                String temp = (String) appointmentList.getSelectedValue();
+                Appointment apt = patientAppts.findAppointment(temp);
+                apt.setStatus(Boolean.FALSE);
+            } else if (e.getSource() == undo) {
+                String temp = (String) appointmentList.getSelectedValue();
+                if (temp == null) {
+                    temp = (String) trueList.getSelectedValue(); 
+                }
+                if (temp == null) {
+                    temp = (String) falseList.getSelectedValue();
+                }
+                Appointment apt = patientAppts.findAppointment(temp);
+                apt.setStatus(null);
+            }
+             updateLists(); 
         }
+
+        private void updateLists() {
+            DefaultListModel model = new DefaultListModel();
+            String[] tempNull = patientAppts.appointmentStatusSort();
+            String[] tempTrue = patientAppts.getTrueList();
+            String[] tempFalse = patientAppts.getFalseList();
+            appointmentList.setModel(resetModel(tempNull));
+            trueList.setModel(resetModel(tempTrue));
+            falseList.setModel(resetModel(tempFalse));
+        }
+
+        private DefaultListModel resetModel(String[] temp) {
+            DefaultListModel model = new DefaultListModel();
+            for (String temp1 : temp) {
+                model.addElement(temp1);
+            }
+            return model;
+        }
+
     }
 
-    private class PatientListener implements ListSelectionListener {
-        @Override
-        public void valueChanged(ListSelectionEvent e) {
-            String selectedName = (String) doctorList.getSelectedValue();
-            System.out.println(selectedName);
-        }
-    }    
-    
 }
